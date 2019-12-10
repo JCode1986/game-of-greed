8072f9cebab75fa6cd7199037229e819c1cff099
Joseph Hangarter <joseph.hangarter@yahoo.com>
2019-12-09 23:54:26 -0800

    greeting, and 1's and 5's calculations somewhat done

--- game_of_greed.py
+++ game_of_greed.py
@@ -1,17 +1,33 @@
 import collections
+import random
 
 class Game:
 
-    dice_roll = ['one', 'two', 'three', 'four', 'five', 'six']
-    # print(collections.Counter(dice_roll))
+    # print(collections.Counter(result_of_roll))
 
     def __init__(self, print_func=print, input_func=input):
         self._print = print_func
         self._input = input_func
+        self.score = 0
+        self.roll = [1, 2, 3, 4, 5, 6]
+
+    def roll_die(self):
+        pass
+
+    def calculate_score(self, lst):
+        for elem in lst:
+            if elem == 1:
+                self.score += 100
+            elif elem == 5:
+                self.score += 50
+        return self.score
+        
+    def bank_score(self):
+        pass
 
     def play(self):
         self._print('Welcome to Game of Greed')
-        question = self._input('Wanna play? ')
+        question = self._input('Wanna play? press y then enter to proceed. ')
         if question == 'y':
             self._print('Great! Check back tomorrow :D')
         else:
@@ -20,3 +36,4 @@ class Game:
 if __name__ == "__main__":
     game = Game()
     game.play()
+    game.calculate_score([1, 2, 3, 4, 1, 2])
